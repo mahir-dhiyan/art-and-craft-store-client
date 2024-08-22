4f4df303f09d3548f7ab51c72971cb132bbaf6ad
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-08-22 22:25:56 +0600

    all art and craft responsive done

--- src/pages/allart/AllArt.jsx
+++ src/pages/allart/AllArt.jsx
@@ -12,30 +12,34 @@ const AllArt = () => {
     //         })
     // }, []);
     const crafts = useLoaderData();
-   
+
     return (
+        <>
+         <div className="bg-[#ECECF2] p-4 rounded-3xl border-dashed border-2 border-[#C56652] text-center m-4">
+                <h2 className="text-5xl font-semibold"> All <span className="text-[#C56652]">Art & Craft</span> Items  </h2>
+            </div>
         <div className="overflow-x-auto">
-            <table className="table ">
-                {/* head */}
+            <table className="table table-xs table-pin-rows table-pin-cols">
                 <thead>
-                    <tr className="bg-[#A95543] text-white md:text-lg">
-                        <th></th>
-                        <th>Item Name</th>
-                        <th>Subcategory</th>
-                        <th>User's Email</th>
-                        <th>Details</th>
+                    <tr className="">
+                        <th className="bg-[#A95543] text-white"></th>
+                        <th className="bg-[#A95543] text-white">Item Name</th>
+                        <th className="bg-[#A95543] text-white">Subcategory</th>
+                        <th className="bg-[#A95543] text-white">User's Email</th>
+                        <th className="bg-[#A95543] text-white">Details</th>
+                        <th className="bg-[#A95543] text-white"></th>
                     </tr>
                 </thead>
                 {
                     crafts.map((craft, index) => (
                         <tbody>
                             {/* row 1 */}
-                            <tr className="hover text-base md:font-medium">
-                                <th>{index + 1}</th>
-                                <td>{craft.item_name}</td>
-                                <td>{craft.subcategory_name}</td>
-                                <td>{craft.email}</td>
-                                <td><Link to={`/viewDetails/${craft._id}`}><button className="btn
+                            <tr className="hover text-base font-medium">
+                                <th >{index + 1}</th>
+                                <td >{craft.item_name}</td>
+                                <td >{craft.subcategory_name}</td>
+                                <td >{craft.name}</td>
+                                <td ><Link to={`/viewDetails/${craft._id}`}><button className="btn
                                  btn-warning bg-[#A95543]
                                  border-none
                                   text-white">View Details</button></Link></td>
@@ -43,8 +47,19 @@ const AllArt = () => {
                         </tbody>
                     ))
                 }
+                <tfoot>
+                <tr className="">
+                        <th className="bg-[#A95543] text-white"></th>
+                        <th className="bg-[#A95543] text-white">Item Name</th>
+                        <th className="bg-[#A95543] text-white">Subcategory</th>
+                        <th className="bg-[#A95543] text-white">User's Email</th>
+                        <th className="bg-[#A95543] text-white">Details</th>
+                        <th className="bg-[#A95543] text-white"></th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
+        </>
     );
 };
 
