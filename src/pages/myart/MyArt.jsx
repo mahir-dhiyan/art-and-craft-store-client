46a27832d2325d403ff30fb2396ffd708287c1fc
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-08-25 13:25:41 +0600

    filter functionality added

--- src/pages/myart/MyArt.jsx
+++ src/pages/myart/MyArt.jsx
@@ -5,8 +5,9 @@ import MyArtCard from "./MyArtCard";
 const MyArt = () => {
     const { user } = useContext(AuthContext);
     const [arts, setArts] = useState([]);
-    const [loading,setLoading]=useState(true);
-   
+    const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState(""); // State for selected filter
+
     useEffect(() => {
         fetch(`http://localhost:5000/art/${user?.email}`)
             .then(result => result.json())
@@ -15,18 +16,40 @@ const MyArt = () => {
                 setLoading(false);
             })
     }, []);
+     // Filter the arts based on the selected customization filter
+     const filteredArts = filter 
+     ? arts.filter(art => art.customization === filter)
+     : arts;
     if (loading) {
         return <div className="mx-auto  flex justify-center h-screen"><span className="loading w-40  mx-auto  loading-infinity "></span></div>
     }
+    console.log(filter);
     return (
         <div className="min-h-screen">
 
             <div className="bg-[#ECECF2] p-4 rounded-3xl border-dashed border-2 border-[#C56652] text-center m-4">
                 <h2 className="text-5xl font-semibold"> My <span className="text-[#C56652]">Art & Craft</span> List</h2>
             </div>
+
+            {/* Dropdown menu for filtering */}
+            <div className="text-center m-4">
+                <label htmlFor="filter" className="mr-2 text-xl font-bold">Filter by Customization:</label>
+                <select
+                    id="filter"
+                    value={filter} 
+                    onChange={(e) => setFilter(e.target.value)}
+                    className="border rounded p-2"
+                >
+                    
+                    <option value="">All</option>
+                    <option value="Yes">Customization: Yes</option>
+                    <option value="No">Customization: No</option>
+                    {/* Add more options as per your customization values */}
+                </select>
+            </div>
             <div className="md:grid md:grid-cols-3 md:gap-5 space-y-4 md:space-y-0 mt-4  ">
                 {
-                    arts.map(art => (
+                    filteredArts.map(art => (
                         <MyArtCard key={art._id} setArts={setArts} arts={arts} art={art}></MyArtCard>
                     ))
                 }
