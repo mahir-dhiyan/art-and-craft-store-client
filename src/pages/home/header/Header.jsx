b35060a2ef0d62709c58e2872527c90a6151882c
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-08-25 17:02:50 +0600

    react typewriter added

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,6 +1,6 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Typewriter } from 'react-simple-typewriter';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -22,22 +22,54 @@ const Header = () => {
                         style={{ backgroundImage: `url(${slider2})` }}
                     >
                         <div className="absolute min-h-80 inset-0 bg-gradient-to-t from-black to-black opacity-30"></div>
-                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'> Rustic Charm, Modern Living</h1></div></SwiperSlide>
+                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>
+                            <Typewriter
+                                words={['Rustic Charm, Modern Living']}
+                                loop={0 | false}
+                                cursor
+                                cursorStyle="_"
+                                typeSpeed={70}
+                                deleteSpeed={10}
+                                delaySpeed={1000}
+                            />
+                        </h1></div></SwiperSlide>
                 <SwiperSlide  >
                     <div
                         className="relative flex  justify-center items-center h-full min-h-80 bg-center bg-cover "
                         style={{ backgroundImage: `url(${slider3})` }}
                     >
                         <div className="absolute  inset-0 bg-gradient-to-t from-black to-black opacity-30"></div>
-                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>Experience the Jute and Wood Magic </h1></div></SwiperSlide>
+                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>
+                            <Typewriter
+                                words={['Experience the Jute and Wood Magic']}
+                                loop={0 | false}
+                                cursor
+                                cursorStyle="_"
+                                typeSpeed={70}
+                                deleteSpeed={10}
+                                delaySpeed={1000}
+                            />
+                        </h1></div></SwiperSlide>
+
                 <SwiperSlide  >
                     <div
                         className="relative flex  justify-center items-center h-full min-h-80 bg-center bg-cover "
                         style={{ backgroundImage: `url(${slider1})` }}
                     >
                         <div className="absolute  inset-0 bg-gradient-to-t from-black to-black opacity-30"></div>
-                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'> Handcrafted Heart, Homely Art</h1></div></SwiperSlide>
-                        
+                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>
+                            <Typewriter
+                                words={['Handcrafted Heart, Homely Art']}
+                                loop={0 | false}
+                                cursor
+                                cursorStyle="__"
+                                typeSpeed={70}
+                                deleteSpeed={10}
+                                delaySpeed={1000}
+                            />
+                        </h1></div></SwiperSlide>
+
+
 
             </Swiper>
         </>
